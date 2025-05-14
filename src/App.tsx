0b94c81441a594b2acf243b12a9c514cc3310b75
yonieeh <martin.vidal@uc.cl>
2025-05-14 03:00:24 -0400

    avance simple landing page

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,12 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import './index.css'
 
 function App() {
   return (
-    <div className="min-h-screen flex items-center justify-center bg-blue-500 text-white text-3xl">
-      Tailwind is working!
+    <div className="text-base sm:text-lg md:text-xl lg:text-2xl xl:text-7xl font-[Comic_Neue] font-bold">
+      Stickman Sigma Chat
     </div>
   );
 }
