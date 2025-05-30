27b1cb11dc1c4860b8714b187059ac169ddc64c6
Martin <martin.vidal@uc.cl>
2025-05-30 15:03:34 -0400

    chore(deploy): added files necessary for netlify deploy

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,5 @@
 import logo from "../assets/logo.png";
-import React from "react";
+import "./Navbar.css";
 
 function Navbar() {
   return (
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { copyFileSync } from 'fs'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      plugins: [{
+        name: 'copy-redirects',
+        writeBundle () {
+          copyFileSync('public/_redirects', 'dist/_redirects');
+        }
+      }]
+    }
+  },
 })
