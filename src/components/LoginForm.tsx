cd5fbc6500089d522a48f7bee6852ffe078e72ed
yonieeh <martin.vidal@uc.cl>
2025-06-17 05:52:38 -0400

    fix: added a few debugging lines for the deploy

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -19,6 +19,7 @@ const LoginForm = () => {
 	const handleSubmit = async(e: React.FormEvent) => {
 		e.preventDefault();
 		try {
+			console.log("Backend URL:", import.meta.env.VITE_BACKEND_URL);
 			const response = await axios.post(`${import.meta.env.VITE_BACKEND_URL}/login`, formData);
 			const result = await response.data;
 
