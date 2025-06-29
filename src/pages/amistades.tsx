bd75955efdcf4db243afdf63bd9eb7413111f3df
yonieeh <martin.vidal@uc.cl>
2025-06-29 06:31:11 -0400

    refactor: changed animations and added a redirect for lack of token in a view

--- src/pages/amistades.tsx
+++ src/pages/amistades.tsx
@@ -1,8 +1,17 @@
 import Navbar from "../components/Navbar";
 import DMView from "../components/dmview";
 import "./amistades.css";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 function Amistades() {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!localStorage.getItem('token')) {
+      navigate('/');
+    }
+  }, []);
 
   return (
     <div className="flex flex-col h-full w-full" style={{ backgroundImage: "url('/comic.png')" }}>
